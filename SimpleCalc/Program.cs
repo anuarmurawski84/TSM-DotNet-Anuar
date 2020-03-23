c2c5b6fa6f2c103b11ee580af7904952fbc878e1
anuarmurawski84 <edart84@mail.ru>
2020-03-24 00:43:11 +0300

    feat: add calculator

---
+++ SimpleCalc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimpleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите цифры для сложения: ");
+            var data1 = Console.ReadLine();
+            var data2 = Console.ReadLine();
+            int iData1 = Convert.ToInt32(data1);
+            int iData2 = Convert.ToInt32(data2);
+            int result = iData1 + iData2;
+            Console.WriteLine($"В результате сложения будет {result}");
+            Console.ReadKey();
+        }
+    }
+}
